6dc7f696b80e7041435429c636b51ca780ee7e78
akshitbansal2005 <155195875+akshitbansal2005@users.noreply.github.com>
2024-05-20 00:53:33 +0530

    Add files via upload

---
+++ script.js code.js
@@ -0,0 +1,22 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a =  prompt("Enter your Number")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+    console.log("Computer is Blasting")
+}
+
+else{
+    console.log("Computer is not Blasting")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+// document.body.style.backgroundColor = "red"
