373739c43221c3432d03b50abd7d60e5165829c9
akshitbansal2005 <155195875+akshitbansal2005@users.noreply.github.com>
2024-07-14 20:05:29 +0530

    Add files via upload

---
+++ script code 44.js
@@ -0,0 +1,42 @@
+console.log("hey this is tutorial 2 of js")
+
+let a = 5;
+a = a + 1
+let b = 6;
+let c = "Harry";
+let _a = "Shubham";
+// var 55a = "rohan"; // Not Allowed
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b,typeof c);
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+// const a1 = 6;
+// a1 = a1 + 1; //Not Allowed because a1 is constant
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crore";
+console.log(o);
