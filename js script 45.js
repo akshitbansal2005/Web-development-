6c211499d2533a457f89949dc60ae88909264cf2
akshitbansal2005 <155195875+akshitbansal2005@users.noreply.github.com>
2024-07-15 19:37:26 +0530

    Add files via upload

---
+++ js script 45.js
@@ -0,0 +1,49 @@
+console.log("hello i am condititonal tutorial")
+
+let age = 1;
+// let grace = 2;
+
+// age += grace 
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+/*
+i am a
+multiline
+comment
+*/
+
+if(age==18){
+    console.log("you can drive");
+}
+
+else if(age==0){
+    console.log("are you kidding");
+}
+
+else if(age==1){
+    console.log("are you again kidding");
+}
+
+else{
+    console.log("you cannot drive");
+}
+
+a=6;
+b=8;
+let c = a>b ?(a-b):(b-a)
+
+/*
+translate to:
+if(a>b){
+    let c = a -b;
+}
+else {
+    let c = a -b;
+}
+*/
