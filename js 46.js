705fd90f14408f4103ff7bcef8547bd92a283084
akshitbansal2005 <155195875+akshitbansal2005@users.noreply.github.com>
2024-07-16 19:51:10 +0530

    Add files via upload

---
+++ js 46.js
@@ -0,0 +1,40 @@
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);
+
+// for(let i = 0; i<100; i++){
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     comapany: "CodeWithHarry AI"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key, element)   
+//     }
+
+// for (const c of "Harry") {
+//     console.log(c)
+    
+// }
+
+// let i = 5;
+// while(i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
+ 
